2f4e01adac992624592846bcc589005fc758acf9
Aldhair <u201315853@upc.edu.pe>
2019-06-06 02:29:40 -0500

    Long ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './app_Screens/home.dart';
+import 'dart:math';
 
 final appName="Exploring UI Widgets";
 
@@ -9,7 +10,15 @@ final appBarColor=Colors.grey[600];
 AssetImage logoAsset=AssetImage('Assets/INART_Logo.png');
 Image logo=Image(image:logoAsset,width: 250.0,height:300.0,);
 
+//List<Colors> listColor= new List<Colors>(4);
+final colorA=Colors.deepPurple;
+final colorB=Colors.amber;
+final colorC=Colors.lightBlue;
+final colorD=Colors.cyan;
+final colors={Colors.deepPurple,Colors.yellow,Colors.red,Colors.lightGreen,Colors.amber,Colors.lightBlue};
 //void main() => runApp(MyApp());
+
+final possibleIcons={Icons.music_note,Icons.videogame_asset,Icons.gamepad,Icons.art_track,Icons.android};
 
 void main(){
   runApp(MaterialApp(
@@ -17,14 +26,15 @@ void main(){
     title:appName,
     //home:Home(),
     home:Scaffold(
-      body:getListView()
+        appBar:AppBar(title: Text("Long List"),),
+        body:getListView()
     )
   ));
 }
 
-Widget getListView(){
+//Inart List
+/*Widget getListView(){
   var listView= ListView(
-
       children: <Widget>[
       logo,
       /*Text(
@@ -76,4 +86,64 @@ Widget getListView(){
     ]
   );
   return listView;
+}*/
+
+//Step 1: Prepare Data Source
+List<String> getListElements(){
+  var items=List<String>.generate(1000,(counter)=>"Item $counter");
+  return items;
+}
+
+//Step 2: Convert Data Source into WidgetsApp
+Widget getListView(){
+  var listItems=getListElements();
+
+  var listView= ListView.builder(
+    itemBuilder: (context,index){
+      return ListTile(
+          leading:Icon(icons()),
+          title:
+            Container(
+              child: Text(
+                  listItems[index],
+                    style: TextStyle(
+                    //color: Colors.deepPurple,
+                    color: color(),)
+              )
+            ),
+          onTap: ()=> debugPrint('${listItems[index]} was tapped'),
+      );
+    },
+  );
+  return listView;
+}
+
+int RandColor(int _val)
+{
+  var value= new Random();
+  return value.nextInt(_val);
+}
+
+MaterialColor color(){
+  MaterialColor temp=Colors.cyan;
+  /*switch(RandColor())
+  {
+    case 0:
+      temp=colorA;
+      break;
+    case 1:
+      temp=colorB;
+      break;
+    case 2:
+      temp=colorC;
+      break;
+    case 3:
+      temp=colorD;
+      break;
+  }*/
+  return colors.elementAt(RandColor(colors.length));
+}
+
+IconData icons(){
+  return possibleIcons.elementAt(RandColor(possibleIcons.length));
 }
